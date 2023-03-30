f375caebfcf576ec451410388f8894fa42752ae0
James Hubert <jwhubert91@gmail.com>
2023-03-29 21:54:36 -0400

    change app structure

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native"
+import React from "react"
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  )
+}
+
+export default IndexScreen
